b9c3c709f794462ad36f04d8de77cfefedbcded1
Pankti2612 <Pankti2612@users.noreply.github.com>
2017-08-19 16:03:41 +0530

    After Changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 var webpack = require('webpack');
 var watch = require('watchpack');
+var path = require('path');
 
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: {
+    './src/index.js',
+  },
   output: {
     path: "./dist",
     filename: 'bundle.js'
   },
   watch: true,
-  module: {
+  module :{
+    exclude: '/node_modules/',
     loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, './src/')
+    }
+  ],
+  query: {
+    query: {presets: ['react', 'es2015', 'stage-1']}
+  }
+},
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx','.css']
   },
   devServer: {
     historyApiFallback: true,
